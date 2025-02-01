297fbfd83932e6c7bf1a9850d09e237413e7d702
kazimehedihasan1 <kazimehedihasan243@gmail.com>
2025-02-01 17:49:04 +0600

    add searchbar

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -6,11 +6,7 @@ const Registration = () => {
     <div className="">
       <div className="bg-[#DA853D] text-white py-8">
         <CommonWrapper>
-          <h2>Registration</h2>
-          <p>Home / Registration</p>
-
-          <h2 className="text-[#303030] text-3xl">Login Form</h2>
-          <p>Already a Member? Log in here.</p>
+         
           <h2 className="py-1 text-4xl font-semibold">Registration</h2>
           <p>Home / Registration</p>
         </CommonWrapper>
